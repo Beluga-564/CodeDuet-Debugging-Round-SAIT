1d1f9e71a520e7cfdb08f1b7d3a4a5c00f5007c6
Beluga-564 <108560010+Beluga-564@users.noreply.github.com>
2023-04-01 18:29:03 +0530

    Update 3_Print_Your_Tree.cpp

--- 3_Print_Your_Tree.cpp
+++ 3_Print_Your_Tree.cpp
@@ -1,108 +1,144 @@
-// CodeDuet 2K23 Round 3
+// CodeDuet 2k23 Round 3
 
-// You are given a basic implementation of Binary Tree, which consists of some errors (Syntactical/Logical). You need to detect those errors and correct so that, they satisfy the given input output conditions.
+// This code is implementation of stack using linked list
+// Debug this code and find the errors and correct them
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-
-  public:
+class Node
+{
+public:
     int data;
-    Node *left, *right;
-
-    Node(int data) {
-      this->data = data;
-      this->left = this->right = NULL;
-    }
-
-    // Function to create a node.
-    Node* add(Node *root, int value) {
-      if(root == NULL) {
-        return new Node(value);
-      }
-
-      if(value < root->data) {
-        root->left = add(root->left,value);
-      }
-      else{
-        root->right = add(root->right,value);
-      }
-      return root;
+    Node *next;
+    Node(int data){
+        this->data=data;
+        next=NULL;
     }
 };
 
-// Function to return preorder traversal of binary tree.
-vector<int> preorder(Node* root) {
-  vector<int> ans;
-  if(root == NULL){
-      return ans;
-  }
+class Stack
+{
+public:
+    Node *top;
+    void display();
+    void push();
+    void pop();
+    void peek();
+    Stack(){
+        top=NULL;
+    }
+};
 
-  queue<Node*> stk;
-  stk.push(root);
+// To display the values present in stack or display "Empty Stack" if stack is empty
+void Stack ::display()
+{
+    if (top == NULL)
+    {
+        cout << "Empty Stack\n";
+    }
+    else
+    {
+        Node *p=top;
 
-  while(!stk.empty()){
-      Node* temp = stk.front();
-      stk.pop();
-
-      if(temp->left){
-          stk.push(temp->left);
-      }
-      if(temp->right){
-          stk.push(temp->right);
-      }
-
-      ans.push_back(temp->data);
-  }
-
-  return ans;
+        while (p)
+        {
+            cout << p->data << "\n";
+            p = p->next;
+        }
+    }
 }
 
-// Function to return postorder traversal of binary tree.
-vector<int> postorderTraversal(Node* root) {
-    vector <int> ans;
-    if(root == NULL){
-        return ans;
+// Push values in the stack
+void Stack ::push()
+{
+    Node *p=NULL;
+    if (top==NULL)
+    {
+        cout << "Enter data: ";
+        int temp;
+        cin >> temp;
+        p=new Node(temp);
+        p->next = top;
+        top = p;
+        display();
     }
-    
-    queue <Node*> q;
-    q.push(root);
-    
-    while(!q.empty()){
-        Node* temp = q.front();
-        q.pop();
-        if(temp->right){
-            q.push(temp->right);
-        }
-        if(temp->left){
-            q.push(temp->left);
-        }
-        ans.push_back(temp->data);
+    else{
+        cout << "Enter data: ";
+        int temp;
+        cin >> temp;
+        p=new Node(temp);
+        p->next = top;
+        top = p;
+        display();
     }
-    reverse(ans.begin(), ans.end());
-    return ans;
+}
+
+// Pop values in the stack
+void Stack::pop()
+{
+    if (top == NULL)
+        cout << "Stack Underflow\n";
+    else
+    {
+        Node *p = top;
+        top = top->next;
+        p->next=NULL;
+        delete p;
+        display();
+    }
+}
+
+// peek values at particular index in the stack
+void Stack::peek()
+{
+    if (top == NULL)
+        cout << "Empty Stack\n";
+    else
+    {
+        cout << "Enter index: ";
+        int x;
+        cin >> x;
+        Node *p = top;
+        if(x==1){
+            cout<<top->data<<endl;
+            return;
+        }
+        for (int i = 1; p && i <= x; i++, p = p->next)
+        {
+            if (i == x){
+                cout << p->data << endl;
+                return;
+            }
+        }
+    }
 }
 
 int main()
 {
-  
-  Node* root = new Node(12);
-
-  // root = root->add(root,12);
-  root = root->add(root,13);
-  root = root->add(root,11);
-  root = root->add(root,10);
-  root = root->add(root,9);
-
-  auto ans1 = preorder(root);
-  for(auto itr:ans1) {
-    cout<<itr<<" ";
-  }cout<<endl;
-
-  auto ans2 = postorderTraversal(root);
-  for(auto itr:ans2) {
-    cout<<itr<<" ";
-  }
-  return 0;
+    Stack s1;
+    while (1)
+    {
+        cout << "MENU: \n1.display\n2.push\n3.pop\n4.peek\n";
+        int choice;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            s1.display();
+            break;
+        case 2:
+            s1.push();
+            break;
+        case 3:
+            s1.pop();
+            break;
+        case 4:
+            s1.peek();
+            break;
+        default:
+            return 0;
+        }
+    }
+    return 0;
 }
